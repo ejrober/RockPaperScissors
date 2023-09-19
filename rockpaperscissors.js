cd15a67c6a1bbcd04c5925d7d19d333ea6efd200
ejrober <25645445+ejrober@users.noreply.github.com>
2023-09-19 14:02:58 -0600

    Functionality complete in game to best of 5

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,25 +1,116 @@
 console.log("Hello, let's play Rock Paper Scissors!");
-var playerSelection = prompt("Enter your choice: ");
 
-function checkUserChoice(input){
-    if(input == "Rock"){
-        console.log("Good! You chose rock!");
-    }
-    else if(input == "Paper"){
-        console.log("Good! You chose paper!");
-    }
-    else if(input == "Scissors"){
-        console.log("Good! You chose scissors!");
-    }
-    else{
-        console.log("Please enter rock, paper, or scissors next time!");
-    }
+
+function getPlayerChoice(){
+    let input = prompt("Rock, paper, Scissors?");
+    switch (input.toLowerCase()) {
+        case "rock":
+            return"rock";
+        case "paper":
+            return "paper";
+        case "scissors":
+            return "scissors";
+        default:
+            return "Bad Input!";
+      }
+      
 }
 
 function getComputerChoice(){
     var randomNumber = Math.floor(Math.random() * 3) + 1;
-    console.log(randomNumber)
+    switch (randomNumber) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            return "Bad option!";
+      }
 }
 
-checkUserChoice(playerSelection)
-getComputerChoice()
+function playRound(playerSelection, computerSelection){
+    switch (playerSelection.toLowerCase()) {
+        case "rock":
+            switch (computerSelection){
+                case "rock":
+                    console.log("You Tie!");
+                    return 0;
+                    
+                case "paper":
+                    console.log("You lose!");
+                    return -1;
+                    
+                case "scissors":
+                    console.log("You win!");
+                    return 1;
+                    
+                default:
+                    return "Something went wrong!";
+                    
+            }
+            
+        case "paper":
+            switch (computerSelection){
+                case "rock":
+                    console.log("You win!");
+                    return 1;
+                    
+                case "paper":
+                    console.log("You Tie!");
+                    return 0;
+                    
+                case "scissors":
+                    console.log("You lose!");
+                    return -1;
+                    
+                default:
+                    return "Something went wrong!";
+                    
+            }
+            
+        case "scissors":
+            switch (computerSelection){
+                case "rock":
+                    console.log("You lose!");
+                    return -1;
+                    
+                case "paper":
+                    console.log("You win!");
+                    return 1;
+                    
+                case "scissors":
+                    console.log("You Tie!");
+                    return 0;
+                    
+                default:
+                    return "Something went wrong!";
+                    
+            }
+           
+        default:
+            return "Bad input!";
+            
+      }
+}
+
+function game(){
+    let i = 0;
+    let wins = 0;
+    while (i < 5) {
+        const playerSelection = getPlayerChoice();
+        const computerSelection = getComputerChoice();
+        wins += playRound(playerSelection, computerSelection);
+        i++;
+    }
+
+    if (wins >=3){
+        console.log("You beat the computer!");
+    }
+    else{
+        console.log("You lost to the computer!");
+    }
+}
+
+game();
